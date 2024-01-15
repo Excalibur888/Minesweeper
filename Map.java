f3fa4123d938a7f9e3a4aec85e4d3fa50ae14606
Selefin <akesse@et.esiea.fr>
2024-01-15 16:51:37 +0100

    Map creation

--- Map.java
+++ Map.java
@@ -1,9 +1,11 @@
 public class Map {
-    private int height;
-    private int width;
-    private Box[][] boxes;
+    final int height;
+    final int width;
+    final Box[][] boxes;
 
     public Map(int height, int width) {
-
+        this.height = height;
+        this.width = width;
+        this.boxes = new Box[height][width];
     }
 }
