8b940f7c2d7197a76e7e6af6001c8fbe27a04927
DUMOND Arthur <dumond@et.esiea.fr>
2024-01-16 14:05:49 +0100

    Game creation added

--- Game.java
+++ Game.java
@@ -1,9 +1,11 @@
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Game {
     private String name;
     private int score;
+    private int minesCount;
     private Map map;
     private LocalDate date;
     private Timer timer;
@@ -16,6 +18,29 @@ public class Game {
         this.difficulty = difficulty;
         this.date = LocalDate.now();
         this.status = Game_Status.WAITING;
+
+        switch (this.difficulty) {
+            case Game_Difficulty.EASY:
+                this.map = new Map(10, 10);
+                this.minesCount = 10;
+                break;
+            case Game_Difficulty.NORMAL:
+                this.map = new Map(20, 20);
+                this.minesCount = 40;
+                break;
+            case Game_Difficulty.HARD:
+                this.map = new Map(30, 30);
+                this.minesCount = 99;
+                break;
+            case Game_Difficulty.PERSONALISED:
+                System.out.println("Enter the map size :\n");
+                Scanner input = new Scanner(System.in);
+                int size = input.nextInt();
+                this.map = new Map(size, size);
+                System.out.println("Home much mines do you want ?\n");
+                this.minesCount = input.nextInt();
+                break;
+        }
     }
 
     public Game(Map map) {
@@ -40,10 +65,6 @@ public class Game {
 
     public Game_Status getStatus() {
         return this.status;
-    }
-
-    public void setDifficulty(Game_Difficulty difficulty) {
-        this.difficulty = difficulty;
     }
 
     public void setStatus(Game_Status status) {
