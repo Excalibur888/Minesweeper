7cc9ce03e66878bf54793251ef18ca4c8142ea20
DUMOND Arthur <dumond@et.esiea.fr>
2024-01-17 12:06:22 +0000

    Revert "Architecture rewiewed"
    This reverts commit 35dbe78b7684fe8d52aab951048018ac09e19957

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main{
-    public static void main(String[] args){
+
+public class Main {
+    public static void main(String[] args) throws Exception{
         Players players = new Players("players.json");
         while (true) {
             Player player = null;
